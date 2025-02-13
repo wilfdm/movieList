cb0c7823f31bf5d9d5adcbfb3e84fa3fc4143341
wilfdm <wilf.d.martinez@gmail.com>
2025-02-12 22:36:33 -0300

    Añadido el query para busqueda, y las tarjetas, con las imegenes de la API

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import Search from "./components/search"
 import Spinner from "./components/Spinner";
+import MovieCard from "./components/MovieCard";
+
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = import.meta.env.VITE_TMBD_API_KEY;
@@ -20,12 +22,14 @@ const App = () => {
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setisLoading] = useState(false);
 
-  const fetchMovies = async () => {
+  const fetchMovies = async (query = '') => {
     setisLoading(true);
     setErrorMessage('');
 
     try {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = query 
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
       
       const response = await fetch(endpoint, API_OPTIONS);
 
@@ -52,8 +56,8 @@ const App = () => {
 
 
   useEffect(() => {
-    fetchMovies();
-  }, []);
+    fetchMovies(searchTerm);
+  }, [searchTerm]);
 
 
 
@@ -83,9 +87,7 @@ const App = () => {
             ) : (
               <ul>
                 {movieList.map((movie) => (
-                  <p key={movie.id} className="text-white">
-                    {movie.title}
-                  </p>
+                  <MovieCard key={movie.id} movie={movie} />
                 ))}
               </ul>
             )}
